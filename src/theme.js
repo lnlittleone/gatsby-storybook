7babde1a101c89282794c18c72fd070085d8c26b
lnlittleone <hmc.esnault@gmail.com>
2019-12-03 18:34:10 +0100

    feat : theme file added

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+export const theme = {
+  main : "#1F1C39",
+  secondaryColor : "#141226",
+  pink : "#EA6694",
+  mint:"#27DEC5",
+  orange : "#F87A7B",
+  yellow: "#FEDB89",
+  purple : "#8745FF",
+  typo : "#FDFDFF",
+
+  mainFont : 'Avenir Next Condensed, Arial',
+
+}
